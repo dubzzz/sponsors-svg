dd174284c2bd4e66bd2f81c154ebcde6752637dc
Nicolas DUBIEN <github@dubien.org>
2022-12-11 21:51:27 +0100

    Basic

---
+++ sponsorkit.config.js
@@ -0,0 +1,32 @@
+import { defineConfig, presets } from "sponsorkit";
+
+export default defineConfig({
+  // Providers configs
+  github: {
+    // SPONSORKIT_GITHUB_TOKEN, Token requires the `read:user` and `read:org` scopes.
+    login: "dubzzz",
+    type: "user",
+  },
+  opencollective: {
+    // SPONSORKIT_OPENCOLLECTIVE_KEY, Create an API key at https://opencollective.com/applications
+    slug: "fast-check",
+  },
+  // Rendering configs
+  width: 800,
+  formats: ["svg"],
+  // Tiers
+  includePrivate: true,
+  tiers: [
+    // Past GitHub sponsors
+    {
+      title: "Past Sponsors",
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    // Default tier
+    {
+      title: "Backers",
+      preset: presets.base,
+    },
+  ],
+});
